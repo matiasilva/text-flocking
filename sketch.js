f9e8fd1137ae50128958a830b2be2ecf2b9bfa10
Matias Silva <mw781@cam.ac.uk>
2020-03-27 16:37:37 +0000

    Cleared file and added basics

--- sketch.js
+++ sketch.js
@@ -1,25 +1,15 @@
-let x = 0,
-  y = 0;
-let grid = 10;
+const pixel_font;
 
-function setup() {
-  createCanvas(500, 500);
-  background(0);
-  stroke(255);
-  strokeWeight(1);
-  // put setup code here
+function preload(){
+  pixel_font = loadFont('Pixeled.ttf');
 }
 
-function draw() {
-  translate(x, y);
-  if (random() < 0.9) {
-    line(0, grid, grid, 0);
-  } else {
-    line(0, 0, grid, grid);
-  }
-  x += grid;
-  if (x > width) {
-    x = 0;
-    y += grid;
-  }
+function setup(){
+
+
 }
+
+function draw(){
+
+}
+
